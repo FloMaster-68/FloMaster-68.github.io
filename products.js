bef3ef9129f2bf9e77bfae5ed80f28c6cf8f7611
FloMaster-68 <flomaster68@mail.ru>
2026-01-14 21:26:47 +0300

    Add files via upload

--- products.js
+++ products.js
@@ -1,24 +1,93 @@
 const PRODUCTS = [
   {
     id: "m1",
-    title: "Тумба «Пример»",
+    title: "Сундук «Лесной 1»",
     category: "Мебель",
     price: 14900,
     currency: "₽",
-    image: "img/m1.jpg",
-    description: "Материалы: ... Размеры: ... Цвет: ...",
+    image: "img/m1/cover.jpg",
+images: [
+      "img/m1/1.jpg",
+      "img/m1/2.jpg"
+    ],
+    description: "Сундук ручной работы. Материалы: ... Размеры: ... Цвет: ...",
+    details: [
+      "Материалы: массив/фанера ...",
+      "Размеры: Ш х Г х В ...",
+      "Покрытие: масло/лак ...",
+      "Срок изготовления: ... дней"
+    ],
     availability: "Под заказ"
   },
+{
+  id: "m2",
+  title: "Сундук «Лесной 2»",
+  category: "Мебель",
+  price: 14900,
+  currency: "₽",
+  image: "img/m2/cover.jpg",
+  images: ["img/m2/1.jpg",
+"img/m2/2.jpg",
+"img/m2/3.jpg"
+],
+  description: "Сундук ручной работы. Материалы: ... Размеры: ... Цвет: ...",
+  details: [
+"Материалы: ...", 
+"Размеры: ...", 
+"Срок изготовления: ... дней"
+],
+  availability: "Под заказ"
+},
+{
+  id: "m3",
+  title: "Консоль деревянная",
+  category: "Мебель",
+  price: 10000,
+  currency: "₽",
+  image: "img/m3/cover.jpg",
+  images: ["img/m3/1.jpg","img/m3/2.jpg","img/m3/3.jpg","img/m3/4.jpg"],
+  description: "Короткое описание (2–3 строки).",
+  details: ["Материалы: ...", "Размеры: ...", "Срок изготовления: ... дней"],
+  availability: "Под заказ"
+},
   {
     id: "d1",
-    title: "Декор «Пример»",
+    title: "Поднос «Серия 1»",
     category: "Декор",
     price: 2900,
     currency: "₽",
-    image: "img/d1.jpg",
-    description: "Материалы: ... Размеры: ...",
+    image: "img/d1/cover.jpg",
+images: [
+      "img/d1/1.jpg",
+      "img/d1/2.jpg"
+    ],
+    description: "Декор ручной работы. Материалы: ... Размеры: ...",
+    details: [
+      "Материалы: ...",
+      "Размеры: ..."
+    ],
     availability: "В наличии"
+  },
+  {
+    id: "d2",
+    title: "Дровница",
+    category: "Декор",
+    price: 2900,
+    currency: "₽",
+    image: "img/d2/cover.jpg",
+images: [
+      "img/d2/1.jpg",
+      "img/d2/2.jpg"
+    ],
+    description: "Декор ручной работы. Материалы: ... Размеры: ...",
+    details: [
+      "Материалы: ...",
+      "Размеры: ...",
+"Срок изготовления: ... дней"
+    ],
+    availability: "Под заказ"
   }
+
 ];
 
 // Ссылка на Telegram (без @)
