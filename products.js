b7a20e2c26517cea8eaba43555f3254c401eadd5
FloMaster-68 <flomaster68@mail.ru>
2026-01-14 16:17:57 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,27 @@
+const PRODUCTS = [
+  {
+    id: "m1",
+    title: "Тумба «Пример»",
+    category: "Мебель",
+    price: 14900,
+    currency: "₽",
+    image: "img/m1.jpg",
+    description: "Материалы: ... Размеры: ... Цвет: ...",
+    availability: "Под заказ"
+  },
+  {
+    id: "d1",
+    title: "Декор «Пример»",
+    category: "Декор",
+    price: 2900,
+    currency: "₽",
+    image: "img/d1.jpg",
+    description: "Материалы: ... Размеры: ...",
+    availability: "В наличии"
+  }
+];
+
+// Ссылка на Telegram (без @)
+const CONTACTS = {
+  telegram: "https://t.me/Flo68Master"
+};
